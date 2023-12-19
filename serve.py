a23e425115837497db3f833835308a218f03b4db
Chilion Snoek <chilionsnoek@gmail.com>
2023-12-19 16:15:34 +0100

    Create serve.py

---
+++ serve.py
@@ -0,0 +1,7 @@
+
+# Define the number of characters
+number_of_characters = 1000
+
+# Loop through each character and simulate an attack
+for character in range(1, number_of_characters + 1):
+    print(f"Character {character} attacked")
